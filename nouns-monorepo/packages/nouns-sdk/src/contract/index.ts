d5daf7ae2eabe17752303462ecb0c60fac12cd74
sherlock-admin <dev@sherlock.xyz>
2024-11-15 14:17:35 +0000

    Copy nouns-monorepo @ d30f27be31321f3e3a5908d588ae3f5c764db7a3

---
+++ nouns-monorepo/packages/nouns-sdk/src/contract/index.ts
@@ -0,0 +1,17 @@
+export { getContractAddressesForChainOrThrow } from './addresses';
+export { getContractsForChainOrThrow } from './contracts';
+export { ChainId, ContractAddresses } from './types';
+export {
+  NounsTokenABI,
+  NounsAuctionHouseABI,
+  NounsDescriptorABI,
+  NounsSeederABI,
+  NounsDAOV3ABI,
+  NounsDAOExecutorV2ABI,
+  NounsTokenFactory,
+  NounsAuctionHouseFactory,
+  NounsDescriptorFactory,
+  NounsSeederFactory,
+  NounsDaoLogicFactory,
+  NounsDaoExecutorV2Factory,
+} from '@nouns/contracts';
